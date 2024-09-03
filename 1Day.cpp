9e4929c57188d488eb6c7e922200d0c4acf313cd
Rajesh22177 <118590473+Rajesh22177@users.noreply.github.com>
2024-09-03 22:51:06 +0530

    Add files via upload

---
+++ 1Day.cpp
@@ -0,0 +1,98 @@
+
+// #include <iostream>
+
+// int main() {
+//     // Write C++ code here
+//     std::cout << "Try programiz.pro";
+
+//     return 0;
+// }
+
+
+// 2
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a, b, sum=0;
+//     cout<<"Enter the first number"<<endl;
+//     cin>>a;
+//     cout<<"Enter the second number"<<endl;
+//     cin>>b;
+//      sum=a+b;
+//   cout<<"The sum is two number is"<<sum<<endl;
+  // }
+
+
+// 3Area os square
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,sum;
+//     cout<<"Enter the two number"<<endl;
+//     cin>>a>>b;
+//      sum=a*b;
+//      cout<<"The area of square is"<<sum;
+// }
+
+
+// 4 Minum of two number
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a, b;
+//     cout<<"Enter the two number"<<endl;
+//     cin>>a>>b;
+//     if(a<b){
+//         cout<<"A is small numbe"<<endl;
+//     }else{
+//         cout<<"b is not small number";
+//     }
+// }
+
+
+//5 is number is even or odd
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     cout<<"Enter the number"<<endl;
+//     cin>>num;
+//     if(num%2==0){
+//         cout<<"this is even number"<<endl;
+//     }else{
+//         cout<<"this is odd number"<<endl;
+//     }
+// }
+
+
+// 6  Sum the number from 1 to N;
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num, i, sum;
+//     cout<<"Enter the number"<<endl;
+//     cin>>num;
+//     for(i=1;i<=num; i++){
+//         sum=sum+i;
+     
+//     }
+//      cout<<"the sum of num is "<< sum;
+// }
+
+// 7  Factorial
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num,i, sum;
+//     cout<<"Enter the number"<<endl;
+//     cin>>num;
+//     for(i=1; i<=num; i++){
+//         sum=sum+i;
+//     }
+//     cout<<"The sum of factorial is  "<<sum;
+// }
+  
+  
